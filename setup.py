02111f48139bb088d7a3f0c26196ad3f76669144
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-03-20 16:13:44 -0700

    renamed everything to sailor

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ def readme():
         return f.read()
 
 
-setup(name='schooner',
+setup(name='sailor',
       version='0.1.0',
       description='Single-cell hub for object-oriented exploratory research',
       long_description='Functions for common single-cell analyses such as '
                        'PCA, clustering, outlier detection, splicing '
                        'modality clustering, bimodal gene expression '
                        'detection.',
-      url='http://github.com/YeoLab/schooner',
+      url='http://github.com/YeoLab/sailor',
       author='Olga Botvinnik',
       author_email='obotvinn@ucsd.edu',
       license='MIT',
