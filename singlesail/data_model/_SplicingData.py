9ef0df3659da077332e389896b83ae54e6d0d851
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-04-08 15:42:38 -0700

    specified SplicingData reducer as NMF

--- singlesail/data_model/_SplicingData.py
+++ singlesail/data_model/_SplicingData.py
@@ -1,17 +1,17 @@
 import numpy as np
 from singlesail.data_model._Data import Data
 from singlesail import compute
-from sklearn.decomposition import PCA
+from sklearn.decomposition import NMF
 
 
 class SplicingData(Data):
     def __init__(self, df, n_components=2, binsize=0.1,
-                 reducer=PCA):
+                 reducer=NMF):
         """Instantiate a object for data scores with binned and reduced data
 
         Parameters
         ----------
-        data : pandas.DataFrame
+        df : pandas.DataFrame
             A [n_events, n_samples] dataframe of splicing events
         n_components : int
             Number of components to use in the reducer
@@ -24,20 +24,18 @@ class SplicingData(Data):
 
         """
         self.df = df
-        self.reducer = reducer
-        # self.psi_fillna_mean = self.data.T.fillna(self.data.mean(axis=1)).T
         self.binsize = binsize
         self.n_components = n_components
         self.binned = compute.binify(self.df, binsize=self.binsize)
-        self.reduce()
+        self.reduce(reducer)
 
-    def reduce(self):
+    def reduce(self, reducer):
         """Reduces dimensionality of the binned df score data
         """
-        self.reducer_fit_to_binned = self.reducer(n_components=self.n_components).fit(self
+        self.reducer = reducer(n_components=self.n_components).fit(self
                                                                     .binned)
-        self.reduced_binned = self.reducer_fit_to_binned.transform(self.binned)
-        if hasattr(self.reducer_fit_to_binned, 'explained_variance_ratio_'):
-            self.plot_explained_variance(self.reducer_fit_to_binned,
+        self.reduced_binned = self.reducer.transform(self.binned)
+        if hasattr(self.reducer, 'explained_variance_ratio_'):
+            self.plot_explained_variance(self.reducer,
                                          '{} on binned data'.format(self.reducer))
         return self
